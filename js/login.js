7aa926397d982b81573e831b38c9bc983405d92f
Kauê Ciglioni <102191780+kaueciglioni@users.noreply.github.com>
2024-05-15 19:47:03 -0300

    Final commit
    Upload do arquivo para visualição e versionamento em git.

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
+
+const validateInput = ({ target }) => {
+    if (target.value.length >2) {
+      button.removeAttribute ('disabled');
+      return;
+    }
+      button.setAttribute ('disabled' , '');
+    }
+
+    const handleSubimt = (event) => {
+        event.preventDefault();
+        
+        localStorage.setItem('player', input.value);
+        window.location = '../html/game.html';
+    }
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubimt);
